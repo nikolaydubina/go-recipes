de51683fd0337b3cd06ccbb8bb3b56f1379d476a
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2023-05-25 21:14:23 +0800

    added links to projects, inlined YAML

--- source/go-simple-webserver.go
+++
@@ -1,5 +0,0 @@
-package main
-
-import "net/http"
-
-func main() { http.ListenAndServe(":9000", http.FileServer(http.Dir("."))) }
--- source/smrcptr.go
+++
@@ -1,7 +0,0 @@
-type Pancake struct{}
-
-func NewPancake() Pancake { return Pancake{} }
-
-func (s *Pancake) Fry() {}
-
-func (s Pancake) Bake() {}
